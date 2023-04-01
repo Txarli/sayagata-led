82e5b6116d6701f16cfe0c37fbff5f45de5be242
Aitor Carlos Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-04-01 17:16:02 +0200

    chore: cambia el puerto

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ fastify.get('/', function (_request, reply) {
   reply.send({ hello: 'world' })
 })
 
-fastify.listen({ port: 3000 }, function (err, _address) {
+fastify.listen({ port: 443 }, function (err, _address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
