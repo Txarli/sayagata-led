a52155289c00b84b6aac0fde40ed4bd73119a1c7
Aitor Carlos Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-04-01 14:21:28 +0200

    chore: añade typescript

--- server.ts
+++ server.ts
@@ -1,14 +1,16 @@
-const fastify = require('fastify')({
-    logger: true
-  })
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
 
 // Declare a route
-fastify.get('/', function (request, reply) {
+fastify.get('/', function (_request, reply) {
   reply.send({ hello: 'world' })
 })
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000 }, function (err, _address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
