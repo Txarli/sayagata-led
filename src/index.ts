70c3faf5e7b05848b8712d6d54bcd53888a4f0a0
Aitor Carlos Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-04-01 18:30:54 +0200

    chore: añade CORS

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
+await fastify.register(cors, {
+  origin: '*',
+});
 
-fastify.get('/hello', function (_request, reply) {
-  reply.send({ hello: 'world' })
-})
+fastify.post("/led-strip", function (_request, reply) {
+  console.log("Recibida llamada de leds 🎉");
+  reply.send(200);
+});
 
 fastify.listen({ port: 10000 }, function (err, _address) {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-})
+});
