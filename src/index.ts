9c783319cab85ba8308bbb94d162dd535bfcf941
Aitor Carlos Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-04-02 13:07:11 +0200

    feat: añade servidor http

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import { Server } from "socket.io";
+import express from "express";
+import http from "http";
 
-const io = new Server(3000, {
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("I'm alive! 🎉")
+})
+
+app.post("/kaixo", (req, res) => {
+  console.log(req.socket.remoteAddress);
+  res.send("Kaixo!");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:8000",
     methods: ["GET", "POST"],
